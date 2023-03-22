5ae3c3ebba3652d3618d533693a20b91c635e7c5
bizhongliang <1183008540@qq.com>
2023-03-22 16:51:56 +0800

    处理category字段不存在时bug

--- task_system_client/task_center/task.py
+++ task_system_client/task_center/task.py
@@ -3,9 +3,17 @@ from datetime import datetime
 
 class Category:
     def __init__(self, category):
-        self.name = category['name']
-        self.parent = Category(category['parent']) if category.get('parent') else None
-        self.config = category.get('config') or {}
+        if category is None:
+            self.name = None
+            self.parent = None
+            self.config = {}
+        else:
+            self.name = category['name']
+            self.parent = Category(category['parent'])
+            self.config = category.get('config') or {}
+
+    def __bool__(self):
+        return bool(self.name)
 
 
 class Task:
@@ -16,6 +24,7 @@ class Task:
         self.category = Category(task['category'])
         self.config = task.get('config') or {}
         self.parent = Task(task['parent']) if task.get('parent') else None
+        self.content = task
 
         parent = self.parent
         names = [self.name]
@@ -24,15 +33,20 @@ class Task:
             parent = parent.parent
         self.unique_name = '-'.join(reversed(names))
 
-        names = [self.category.name]
-        parent = self.category.parent
-        while parent:
-            names.append(parent.name)
-            parent = parent.parent
-        self.unique_category = '-'.join(reversed(names))
+        if self.category:
+            names = [self.category.name]
+            parent = self.category.parent
+            while parent:
+                names.append(parent.name)
+                parent = parent.parent
+            self.unique_category = '-'.join(reversed(names))
+        else:
+            self.unique_category = None
 
     def __str__(self):
         return 'Task(id=%s, name=%s)' % (self.id, self.unique_name)
+
+    __repr__ = __str__
 
 
 class TaskSchedule:
@@ -48,6 +62,8 @@ class TaskSchedule:
         return 'TaskSchedule(id=%s, time=%s, task=%s)' % (
             self.schedule_id, self.schedule_time, self.task
         )
+
+    __repr__ = __str__
 
     def __hash__(self):
         return hash("%s-%s" % (self.schedule_id, self.schedule_time))

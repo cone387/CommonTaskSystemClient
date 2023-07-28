362677ddfed3ce49a15c2f6ddb347dfe4c213736
bizhongliang <1183008540@qq.com>
2023-07-28 18:00:08 +0800

    自动判断日志上传

--- task_system_client/callback/callbacks/upload_log.py
+++ task_system_client/callback/callbacks/upload_log.py
@@ -5,26 +5,28 @@ import json
 from task_system_client.utils import url as url_utils
 
 
-@Callback()
-class HttpUploadLogCallback(BaseCallback):
+class LogEngine:
 
+    def upload(self, log):
+        raise NotImplementedError
+
+
+class HttpLogEngine(LogEngine):
     name = 'Http日志上报'
 
-    def run(self):
-        log = self.executor.generate_log()
+    def upload(self, log):
         try:
             res = requests.post(
                 url=settings.LOG_UPLOAD_URL,
                 headers=None,
-                json=self.executor.generate_log(),
+                json=log,
             )
             settings.logger.info('HttpUploadLogCallback: %s', res.text)
         except Exception as e:
             settings.logger.exception('HttpUploadLogCallback error: %s -> %s', log, e)
 
 
-@Callback()
-class RedisUploadLogCallback(BaseCallback):
+class RedisLogEngine(LogEngine):
     name = 'Redis日志上报'
     _client = None
     queue = None
@@ -41,10 +43,30 @@ class RedisUploadLogCallback(BaseCallback):
             self._client = redis.Redis(connection_pool=pool, decode_responses=True)
         return self._client
 
-    def run(self):
-        log = self.executor.generate_log()
+    def upload(self, log):
         try:
             r = self.client.lpush(self.queue, json.dumps(log))
             settings.logger.info('RedisUploadLogCallback -> %s: %s', self.queue, r)
         except Exception as e:
             settings.logger.exception('RedisUploadLogCallback error: %s -> %s', log, e)
+
+
+@Callback()
+class LogUploadCallback(BaseCallback):
+    name = '日志上报'
+
+    _engine = None
+
+    @property
+    def engine(self):
+        if not self._engine:
+            if settings.LOG_UPLOAD_URL.startswith('http'):
+                LogUploadCallback._engine = HttpLogEngine()
+            elif settings.LOG_UPLOAD_URL.startswith('redis'):
+                LogUploadCallback._engine = RedisLogEngine()
+            else:
+                raise ValueError('could not find log engine for %s' % settings.LOG_UPLOAD_URL)
+        return self._engine
+
+    def run(self):
+        self.engine.upload(self.executor.generate_log())
